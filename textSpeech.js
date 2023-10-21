e67696a60c30e42b722b6f68747a173d5e270cc2
Mubarak <mubee2004@gmail.com>
2023-10-21 17:34:49 +0100

    Made a server for calls

--- textSpeech.js
+++ textSpeech.js
@@ -4,7 +4,8 @@ import fs from "fs";
 const apiKey = process.env.XI_API_KEY; // Your API key from Elevenlabs
 const voiceID = "pNInz6obpgDQGcFmaJgB"; // The ID of the voice you want to get
 const fileName = "audio.mp3"; // The name of your audio file
-const textInput = "Yane! Yakake? Ya yau? Ya jindadi?"; // The text you wish to convert to speech
+const textInput =
+  "Hello, this is peace pal. Our call feature is currently under development, but we will make sure we let you know when our call feature becomes available to users! Thank you."; // The text you wish to convert to speech
 
 voice.textToSpeechStream(apiKey, voiceID, textInput).then((res) => {
   res.pipe(fs.createWriteStream(fileName));
